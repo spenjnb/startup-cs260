1dc259651ab34891eff2615ad5500cdb3754fe40
Spencer <spenjnb@gmail.com>
2023-03-14 17:11:13 -0600

    added some ideas and tested code and html

---
+++ ideas.js
@@ -0,0 +1,9 @@
+const json = localStorage.getItem("form");
+const obj = JSON.parse(json);
+
+for (key in obj) {
+    const markup = `<div>
+    <span>${key}: ${obj[key]}</span>
+    </div>`;
+    document.getElementById('data').innerHTML += markup;
+}
