ee951a45f3aebb3d9b2ab1b58404b7ab7a31aa55
Spencer <spenjnb@gmail.com>
2023-03-14 23:23:51 -0600

    worked a little more on the logic and algorithm

--- ideas.js
+++ ideas.js
@@ -1,9 +1,30 @@
 const json = localStorage.getItem("form");
 const obj = JSON.parse(json);
 
+const div = document.createElement("div");
+const span = document.createElement("span")
+
+let array;
+
 for (key in obj) {
     const markup = `<div>
     <span>${key}: ${obj[key]}</span>
     </div>`;
     document.getElementById('data').innerHTML += markup;
 }
+
+let bestMatch = ['outdoor', 'Home', 'places'];
+let option2 = ['groups', 'alone', 'date'];
+let option3 = ['Parks', 'Movies', 'new'];
+let preference = ['morning', 'afternoon', 'evening'];
+let budget = ['cheap','medium', 'expensive'];
+
+for (key in obj) {
+    console.log(key);
+    if (key == "Best Match") {
+        bestMatch.find()
+        console.log(obj[key]);
+        
+    }
+}
+
