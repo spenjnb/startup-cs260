abf03e14404d43a14d4c5ee58bcb1321223a11f5
Spencer <spenjnb@gmail.com>
2023-03-15 23:19:25 -0600

    added two function that will give ideas.

--- ideas.js
+++ ideas.js
@@ -4,8 +4,6 @@ const obj = JSON.parse(json);
 const div = document.createElement("div");
 const span = document.createElement("span")
 
-let array;
-
 for (key in obj) {
     const markup = `<div>
     <span>${key}: ${obj[key]}</span>
@@ -13,18 +11,74 @@ for (key in obj) {
     document.getElementById('data').innerHTML += markup;
 }
 
-let bestMatch = ['outdoor', 'Home', 'places'];
-let option2 = ['groups', 'alone', 'date'];
-let option3 = ['Parks', 'Movies', 'new'];
-let preference = ['morning', 'afternoon', 'evening'];
-let budget = ['cheap','medium', 'expensive'];
+let objectArray = obj;
+let userPreferecnes = new Array();
+
+console.log(objectArray)
 
 for (key in obj) {
     console.log(key);
-    if (key == "Best Match") {
-        bestMatch.find()
-        console.log(obj[key]);
-        
+    if (key !== "Username") {
+        userPreferecnes.push(obj[key]);           
     }
 }
 
+for (let i = 0; i < userPreferecnes.length; ++i) {
+    console.log(userPreferecnes[i]);
+}
+
+function chosenOptions(options) {
+    let locationArray = ['I like the outdoors','I like staying Home','I like to go places'];
+    let personalityArray = ['I like big groups','I want to go alone','I am looking for a date'];
+    let activityArray = ['Parks, lakes, hikes','Movies, cooking, games','Try something new'];
+    let timeArray = ['Morning','Afternoon','Evening'];
+    let budgetArray = ['cheap','medium','expensive'];
+    let packageResult = new Array();
+
+    for (let i = 0; i < 3; ++i) {
+        if (options[0] === locationArray[i]) {
+            packageResult.push(i);
+            console.log(i);
+        }
+    }
+
+    for (let i = 0; i < 3; ++i) {
+        if (options[1] === personalityArray[i]) {
+            packageResult.push(i);
+            console.log(i);
+        }
+    }
+
+    for (let i = 0; i < 3; ++i) {
+        if (options[2] === activityArray[i]) {
+            packageResult.push(i);
+            console.log(i);
+        }
+    }
+
+    for (let i = 0; i < 3; ++i) {
+        if (options[3] === timeArray[i]) {
+            packageResult.push(i);
+            console.log(i);
+        }
+    }
+
+    for (let i = 0; i < 3; ++i) {
+        if (options[4] === budgetArray[i]) {
+            packageResult.push(i);
+            console.log(i);
+        }
+    }
+    return packageResult;
+}
+
+let preferences = chosenOptions(userPreferecnes);
+
+for (let i = 0; i < preferences.length; ++i) {
+    console.log(preferences[i]);
+}
+
+// funtion will implement main logic, it will give ideas based on user input
+function suggestionNumberOne(userSelection) {
+
+}
